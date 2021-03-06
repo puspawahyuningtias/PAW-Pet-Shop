fcda1e6c0c341266cdc65e90d02ac6bcf7cba49d
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-03-06 12:25:55 +0700

    add SplashScreen

---
+++ PAW/app/src/main/java/com/puspawahyuningtias/paw/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.puspawahyuningtias.paw
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val timerThread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@SplashScreen, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+        timerThread.start()
+    }
+    override fun onPause() { // TODO Auto-generated method stub
+        super.onPause()
+        finish()
+    }
+}
